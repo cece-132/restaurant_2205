aa4bc749f1a8e9b3425f91f57c31e8d0a81c8ab0
Candace Eckels <candace.eckels@gmail.com>
2023-04-14 23:33:26 -0600

    completed code and tests for announce_closing_time

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,14 +4,23 @@ class Restaurant
     attr_reader :dishes, :name, :opening_time, :closed, :closing_announce_time
     def initialize(opening_time, name)
       @name = name
-      @opening_time = opening_time
+      @opening_time = format(opening_time)
       @dishes = []
-      # @closed = closed
-      @closing_announce_time = closing_announce_time
+    end
+
+    def format(time)
+      if time.length == 5
+        time
+      else
+        if time[0..1].to_i < 10
+          hours = time[0..1].to_i.to_s.rjust(2, '0')
+          "#{hours}:#{time[-2..-1]}"
+        end
+      end
     end
 
     def closing_time(hours)
-      minutes = @opening_time[3..4]
+      minutes = @opening_time[-2..-1]
       hour = @opening_time[0..1].to_i
       "#{(hour + hours).to_s}:#{minutes}"
     end
@@ -21,7 +30,7 @@ class Restaurant
     end
 
     def open_for_lunch?
-      if opening_time.to_i < 12
+      if opening_time[0..1] < '12'
        true
       else
        false
@@ -29,31 +38,21 @@ class Restaurant
     end
 
     def menu_dish_names
-      menu = []
-      @dishes.each do |dish|
-        menu << dish.upcase
+      if @opening_time[0..1] < '12'
+        @dishes.map { |dish| dish.upcase }
+      elsif @opening_time[0..1] > '12'
+        @dishes
       end
-      menu
     end
 
     def announce_closing_time(hours)
-      time = @opening_time.to_i + hours
-      if time > 24
-        new_int = @closed - 24
-        binding.pry
-        if new_int > 12
-          @closing_announce_time = "#{new_int - 12}:00PM"
-          "#{@name} will be closing at #{closing_announce_time}"
-        else
-           @closing_announce_time = "#{new_int}:00AM"
-           "#{@name} will be closing at #{closing_announce_time}"
-        end
-      elsif time >= 12
-        @closing_announce_time = "#{time - 12}:00PM"
-        "#{@name} will be closing at #{closing_announce_time}"
-      else
-        @closing_announce_time = "#{time}:00AM"
-        "#{@name} will be closing at #{closing_announce_time}"
+      minutes = closing_time(hours)[-2..-1]
+      hour = closing_time(hours)[0..1].to_i
+      if hour < 12
+        "#{@name} will be closing at #{closing_time(hours)}AM"
+      elsif hour > 12
+        hour = hour - 12
+        "#{@name} will be closing at #{hour}:#{minutes}PM"
       end
     end
 
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Restaurant do
     end
   end
 
-  describe 'closing_time(12_hr_time)' do
+  describe '#closing_time(12_hr_time)' do
     it "takes in the number of hours open and adds the number to the opening time" do
       expect(@restaurant1.closing_time(8)).to be_a String
 
@@ -41,42 +41,55 @@ RSpec.describe Restaurant do
       restaurant.add_dish('Ravioli')
 
       expect(restaurant.dishes.count).to eq 3
-      expect(restaurant.dishes).to eq(['Burrata', 'Pizzetta', 'Ravioli'])
+      expect(restaurant.dishes).to eq(["Burrata", "Pizzetta", "Ravioli"])
     end
   end
 
-  describe "Iteration 3" do
-
-    it 'is open for lunch' do
+  describe '#open_for_lunch?' do
+    it 'if open before 12:00 returns true' do
       restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-      restaurant2 = Restaurant.new('16:00', 'Il Posto')
       
       expect(restaurant1.open_for_lunch?).to eq(true)
-      expect(restaurant2.open_for_lunch?).to eq(false)
     end
 
-    it 'can add_dish to menu' do
-      restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+    it 'if open after 12:00 returns false' do
       restaurant2 = Restaurant.new('16:00', 'Il Posto')
 
-      restaurant2.add_dish('Burrata')
-      restaurant2.add_dish('Pizzetta')
-      restaurant2.add_dish('Ravioli')
-
-      expect(restaurant2.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
+      expect(restaurant2.open_for_lunch?).to eq(false)
     end
-
   end
 
-  describe "Iteration 4" do
+  
+  describe '#menu_dish_names' do
+    it 'opening time > 12 returns dishes in the downcase position' do
+      restaurant = Restaurant.new('16:00', 'Il Poggio')
 
-    it 'can announce closing time' do
+      restaurant.add_dish('Burrata')
+      restaurant.add_dish('Pizzetta')
+      restaurant.add_dish('Ravioli')
+
+      expect(restaurant.menu_dish_names).to eq(["Burrata", "Pizzetta", "Ravioli"])
+    end
+
+    it 'opening time < 12 returns dishes in the upcase position' do
+      restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+
+      restaurant.add_dish('Burrata')
+      restaurant.add_dish('Pizzetta')
+      restaurant.add_dish('Ravioli')
+
+      expect(restaurant.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
+    end
+  end
+
+  describe "#announce_closing_time" do
+    it 'change the format of the end time to a 12-hr format' do
       restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
       restaurant2 = Restaurant.new('16:00', 'Il Posto')
 
+      expect(restaurant1.announce_closing_time(5)).to be_a String
       expect(restaurant1.announce_closing_time(5)).to eq("Fuel Cafe will be closing at 11:00AM")
       expect(restaurant2.announce_closing_time(7)).to eq("Il Posto will be closing at 11:00PM")
     end
-
   end
 end
